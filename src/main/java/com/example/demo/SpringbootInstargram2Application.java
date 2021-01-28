a7c9d1dd7c7f4ee6fa5b71d7b9c8f57022bb4256
kimsungsoo <>
2021-01-29 00:23:44 +0900

    jsp 까지만 연동함

---
+++ src/main/java/com/example/demo/SpringbootInstargram2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInstargram2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInstargram2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/webController/ViewController.java
@@ -0,0 +1,13 @@
+package com.example.demo.webController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class ViewController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
